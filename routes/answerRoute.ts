9e3672282d0df1a12291f699d42f03be7b97e28f
OlyaKyrylyuk <olya_kyrylyuk15@ukr.net>
2021-03-12 18:39:04 +0200

    eslint fixes

--- routes/answerRoute.ts
+++ routes/answerRoute.ts
@@ -7,33 +7,34 @@ import Quiz from "./../models/quiz";
 
 const router = express.Router();
 
-router.route("/add/:id")
-.post(async (req: Request, res: Response) => {
-    let Array_data:any = [];
-    let questions =  Quiz.findOne({ _id: req.params.id }).populate("questions")
-    .then((data)=>{
-        if(data !== null){     
-            data.questions.forEach((res, index) => {
-            const answer_data = new Answer({
-                answerContent: req.body.answers[index],
-                question_id: res._id,
-              });
-              Array_data.push(answer_data)
-            })
-        }       
-    }).then(()=>{
-        Array_data.forEach((arr:any) => {
-            arr.save();
-            Question.findByIdAndUpdate(
-                arr.question_id,
-                { $push: { answers: arr } },
-                { new: true, useFindAndModify: false }
-              );
-            })
-    }).then(()=>{
-        res.redirect("/quizes");
-    }       
-    )
+router.route("/add/:id").post(async (req: Request, res: Response) => {
+  let Array_data: any = [];
+  let questions = Quiz.findOne({ _id: req.params.id })
+    .populate("questions")
+    .then((data) => {
+      if (data !== null) {
+        data.questions.forEach((res, index) => {
+          const answer_data = new Answer({
+            answerContent: req.body.answers[index],
+            question_id: res._id,
+          });
+          Array_data.push(answer_data);
+        });
+      }
+    })
+    .then(() => {
+      Array_data.forEach((arr: any) => {
+        arr.save();
+        Question.findByIdAndUpdate(
+          arr.question_id,
+          { $push: { answers: arr } },
+          { new: true, useFindAndModify: false }
+        );
+      });
+    })
+    .then(() => {
+      res.redirect("/quizes");
+    });
 });
 
 export { router as AnswerRouter };
